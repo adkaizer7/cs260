c4ac9e5a06a28de2c82a0e59e0c6469edb110016
Jesmin Ngo <xjesminngo@berkeley.edu>
2015-04-13 17:48:40 -0700

    update

--- cs260-multScreens/src/buttonConfigureExistingDevice.js
+++ cs260-multScreens/src/buttonConfigureExistingDevice.js
@@ -3,14 +3,13 @@ exports.ConExiDev = BUTTONS.Button.template(function($){
 		left: 40, 
 		right: 40, 
 		height:40,
-		bottom:40,
 		opacity : 0,
 		skin : greenSkin,
 		contents: [
 					Container($, 
 						{ 
 							   left: 0, right: 0, height: 40, bottom: 0, active: true, 
-							   skin: whiteSkin, 
+							   skin: greenSkin, 
 							   behavior: Object.create((MainScreen.behaviors[1]).prototype), 
 							   contents: 
 							   [
