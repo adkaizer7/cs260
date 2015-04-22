a54e2f066832aeb74af10d91584ec85038f6747b
Jesmin Ngo <xjesminngo@berkeley.edu>
2015-04-21 23:33:20 -0700

    button interactivity

--- MedInfo/src/main.js
+++ MedInfo/src/main.js
@@ -7,22 +7,32 @@ var headerStyle = new Style({font:"30px Avenir Heavy", color:"black"});
 var textStyle = new Style({font:"22px Avenir", color:"black"});
 var whiteSkin = new Skin({fill:"#ecf0f1"});
 var silverSkin = new Skin({fill:"#bdc3c7"});
-var lightGreenSkin = new Skin({fill: "#83c88b"});
-var darkGreenSkin = new Skin({fill: "#27ae60"});
+var greenSkin = new Skin({fill: "#27ae60"});
+var greenPressSkin = new Skin({fill: "#64bc88"});
+var blueSkin = new Skin({fill: "#2980b9"});
+var bluePressSkin = new Skin({fill: "#5794b5"});
 
 var ScreenBackButton = BUTTONS.Button.template(function($){ return{
-	top: 10, bottom: 10, left: 10, right: 10, 
+	top: 10, bottom: 10, left: 10, right: 10, skin: $.skin,
 	contents: [
 		new Line({top: 0, left: 0, right: 0, bottom: 0, contents: [
-		new Picture({left:0, right:0, top:0, bottom:0, height: 1, url:"back.png"}),
-		new Label({top: 0, bottom: 0, left: 0, right: 100, string: "Back", style: titleStyle}),
+			new Label({top: 0, bottom: 0, left: 0, right: 0, string: $.textForLabel, style: titleStyle}),
 		],
 		})
 	],
 	behavior: Object.create(BUTTONS.ButtonBehavior.prototype, {
-		onTap: { value: function(content){
+		onCreate: { value: function(content){
+			this.upSkin = $.skin;
+			this.downSkin = $.darkSkin;
+		}},
+		onTouchBegan: { value: function(content){
+			content.skin = this.downSkin;
 			trace("backButton was tapped.\n");
-			//application.behavior.switchScreen(HomeScreen);
+		}},
+		onTouchEnded: { value: function(content){
+			content.skin = this.upSkin;
+		}},
+		onComplete: { value: function(content, message, json){
 		}},
 	})
 }});
@@ -30,25 +40,33 @@ var ScreenBackButton = BUTTONS.Button.template(function($){ return{
 var pillsLabel = new Label({top: 0, bottom: 0, left: 0, right: 0, string:"Amount left: 100%", style: textStyle}),
 
 var refillButton = BUTTONS.Button.template(function($){ return{
-	height: 10, top: 10, bottom: 10, left: 10, right: 10, skin: darkGreenSkin,
+	height: 10, top: 10, bottom: 10, left: 10, right: 10, skin: $.skin, 
 	contents: [
-		new Label({left:0, right:0, height:40, string:"> Refill Prescription", style: titleStyle})
+		new Label({left:0, right:0, height:40, string: $.textForLabel, style: titleStyle})
 	],
 	behavior: Object.create(BUTTONS.ButtonBehavior.prototype, {
-		onTap: { value: function(content){
+		onCreate: { value: function(content){
+			this.upSkin = $.skin;
+			this.downSkin = $.darkSkin;
+		}},
+		onTouchBegan: { value: function(content){
+			content.skin = this.downSkin;
 			trace("refillButton was tapped.\n");
-			content.invoke(new Message(deviceURL + "refillMed"), Message.JSON); //NEED TO IMPLEMENT move back to 100%
+			//content.invoke(new Message(deviceURL + "refillMed"), Message.JSON); //NEED TO IMPLEMENT move back to 100%
+		}},
+		onTouchEnded: { value: function(content){
+			content.skin = this.upSkin;
 		}},
 		onComplete: { value: function(content, message, json){
 			pillsLabel.string = "Pill Amount: " + json.food.toFixed(2) + "%";
-		}}
+		}},
 	})
 }});
 
 var MedInfoScreen = new Column({
 		left:0, right:0, top:0, bottom:0, skin: silverSkin,
 		contents:[
-			new ScreenBackButton(),
+			new ScreenBackButton({skin: blueSkin, darkSkin: bluePressSkin, textForLabel: "< Back"}),
 			
 			new Column({ //pill button, name, amount
 				left:10, right:10, top:0, bottom:0,
@@ -71,7 +89,7 @@ var MedInfoScreen = new Column({
 				skin:whiteSkin,
 						contents:[
 								new Label({top: 0, bottom: 0, left: 10, right: 10, string:"Frequency to Take", style: headerStyle}),
-								new Label({top: 0, bottom: 0, left: 10, right: 10, string:"every morning by mouth with milk", style: textStyle}),
+								new Label({top: 0, bottom: 0, left: 10, right: 10, string:"every morning by mouth with food", style: textStyle}),
 						]}),
 						
 			new Column({ //side effects
@@ -81,7 +99,7 @@ var MedInfoScreen = new Column({
 								new Label({top: 0, bottom: 0, left: 10, right: 10, string:"Side Effects", style: headerStyle}),
 								new Label({top: 0, bottom: 0, left: 10, right: 10, string:"drowsiness, nausea", style: textStyle}),
 						]}),
-			new refillButton(),
+			new refillButton({skin: greenSkin, darkSkin: greenPressSkin, textForLabel: "Refill Prescription"}),
 ]});
 
 application.add(MedInfoScreen);
--- doctorInfo/src/main.js
+++ doctorInfo/src/main.js
@@ -7,44 +7,62 @@ var headerStyle = new Style({font:"30px Avenir Heavy", color:"black"});
 var textStyle = new Style({font:"22px Avenir", color:"black"});
 var whiteSkin = new Skin({fill:"#ecf0f1"});
 var silverSkin = new Skin({fill:"#bdc3c7"});
-var lightGreenSkin = new Skin({fill: "#83c88b"});
-var darkGreenSkin = new Skin({fill: "#27ae60"});
+var greenSkin = new Skin({fill: "#27ae60"});
+var greenPressSkin = new Skin({fill: "#64bc88"});
+var blueSkin = new Skin({fill: "#2980b9"});
+var bluePressSkin = new Skin({fill: "#5794b5"});
 
 var ScreenBackButton = BUTTONS.Button.template(function($){ return{
-	top: 10, bottom: 10, left: 10, right: 10, 
+	top: 10, bottom: 10, left: 10, right: 10, skin: $.skin,
 	contents: [
 		new Line({top: 0, left: 0, right: 0, bottom: 0, contents: [
-		new Picture({left:0, right:0, top:0, bottom:0, height: 1, url:"back.png"}),
-		new Label({top: 0, bottom: 0, left: 0, right: 100, string: "Back", style: titleStyle}),
+			new Label({top: 0, bottom: 0, left: 0, right: 0, string: $.textForLabel, style: titleStyle}),
 		],
 		})
 	],
 	behavior: Object.create(BUTTONS.ButtonBehavior.prototype, {
-		onTap: { value: function(content){
+		onCreate: { value: function(content){
+			this.upSkin = $.skin;
+			this.downSkin = $.darkSkin;
+		}},
+		onTouchBegan: { value: function(content){
+			content.skin = this.downSkin;
 			trace("backButton was tapped.\n");
-			//application.behavior.switchScreen(HomeScreen);
+		}},
+		onTouchEnded: { value: function(content){
+			content.skin = this.upSkin;
+		}},
+		onComplete: { value: function(content, message, json){
 		}},
 	})
 }});
 
 var callButton = BUTTONS.Button.template(function($){ return{
-	height: 10, top: 10, bottom: 10, left: 10, right: 10, skin: darkGreenSkin,
+	height: 10, top: 10, bottom: 10, left: 10, right: 10, skin: $.skin, 
 	contents: [
-		new Label({left:0, right:0, height:40, string:"> Call Doctor's Office", style: titleStyle})
+		new Label({left:0, right:0, height:40, string: $.textForLabel, style: titleStyle})
 	],
 	behavior: Object.create(BUTTONS.ButtonBehavior.prototype, {
-		onTap: { value: function(content){
+		onCreate: { value: function(content){
+			this.upSkin = $.skin;
+			this.downSkin = $.darkSkin;
+		}},
+		onTouchBegan: { value: function(content){
+			content.skin = this.downSkin;
 			trace("callButton was tapped.\n");
 		}},
+		onTouchEnded: { value: function(content){
+			content.skin = this.upSkin;
+		}},
 		onComplete: { value: function(content, message, json){
-		}}
+		}},
 	})
 }});
 
 var ApptmtsInfoScreen = new Column({
 		left:0, right:0, top:0, bottom:0, skin: silverSkin,
 		contents:[
-			new ScreenBackButton(),
+			new ScreenBackButton({skin: blueSkin, darkSkin: bluePressSkin, textForLabel: "< Back"}),
 			
 			new Column({ //pill button, name, amount
 				left:10, right:10, top:0, bottom:0,
@@ -62,7 +80,6 @@ var ApptmtsInfoScreen = new Column({
 								new Label({top: 0, bottom: 0, left: 10, right: 10, string:"Address", style: headerStyle}),
 								new Label({top: 0, bottom: 0, left: 10, right: 10, string:"2160 UX Way Berkeley, CA 94704", style: textStyle}),
 						]}),
-						
 			new Column({ //frequency
 				left:10, right:10, top: 10, bottom: 0,
 				skin:whiteSkin,
@@ -70,7 +87,7 @@ var ApptmtsInfoScreen = new Column({
 								new Label({top: 0, bottom: 0, left: 10, right: 10, string:"Notes", style: headerStyle}),
 								new Label({top: 0, bottom: 0, left: 10, right: 10, string:"He's really nice", style: textStyle}),
 						]}),
-			new callButton(),
+			new callButton({skin: greenSkin, darkSkin: greenPressSkin, textForLabel: "Call Doctor's Office"}),
 ]});
 
 application.add(ApptmtsInfoScreen);
