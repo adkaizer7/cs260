318fdb0def698d6b2fc7f0e17525293f60d5a348
Jesmin Ngo <xjesminngo@berkeley.edu>
2015-04-22 19:59:56 -0700

    redesign 1st screen

--- intPrototypePhone/src/buttonLogin.js
+++ intPrototypePhone/src/buttonLogin.js
@@ -1,24 +1,24 @@
 // KPR Script file
 exports.LoginScreen = BUTTONS.Button.template(function($){ 
 	return{
-		left: 40, 
-		right: 40, 
-		height:40,
-		bottom:120,
+		left: 10, 
+		right: 10, 
+		bottom:10,
+		top: 10, 
 		opacity : 0,
 		skin : greenSkin,
 		contents: [
 					Container($, 
 						{ 
-							   left: 0, right: 0, height: 40, bottom: 0, active: true, 
+							   left: 0, right: 0, bottom: 0, top: 0, active: true, 
 							   skin: whiteSkin, 
 							   behavior: Object.create((MainScreen.behaviors[1]).prototype), 
 							   contents: 
 							   [
 									Label($, 
 									{ 
-										left: 0, right: 0, top: 0, bottom: 0, style: textStyle, skin:greenSkin, 
-									    string: 'Returning User? Login', id : 1, 
+										left: 0, right: 0, top: 0, bottom: 0, style: titleStyle, skin:greenSkin, 
+									    string: 'Returning? Login', id : 1, 
 									}),									
 								], 
 						}),
--- intPrototypePhone/src/buttonSignUp.js
+++ intPrototypePhone/src/buttonSignUp.js
@@ -1,24 +1,24 @@
 // KPR Script file
 exports.SignUpScreen = BUTTONS.Button.template(function($){ 
 	return{
-		left: 40, 
-		right: 40, 
-		height:40,
-		bottom:80,
+		left: 10, 
+		right: 10, 
+		bottom:10,
+		top: 10, 
 		opacity : 0,
 		skin : greenSkin,
 		contents: [
 					Container($, 
 						{ 
-							   left: 0, right: 0, height: 40, bottom: 0, active: true, 
+							   left: 0, right: 0, bottom: 0, top: 0, active: true, 
 							   skin: whiteSkin, 
 							   behavior: Object.create((MainScreen.behaviors[2]).prototype), 
 							   contents: 
 							   [
 									Label($, 
 									{ 
-										left: 0, right: 0, top: 0, bottom: 0, style: textStyle, skin:greenSkin, 
-									    string: 'New User? Sign Up', 
+										left: 0, right: 0, top: 0, bottom: 0, style: titleStyle, skin:greenSkin, 
+									    string: 'New? Sign Up', 
 									}),									
 								], 
 						}),
