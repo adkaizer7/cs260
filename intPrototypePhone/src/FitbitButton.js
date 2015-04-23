88bfbc26a36491c7cb44bdea7c92ac28026dfe3e
Phillip Azar <dain54@gmail.com>
2015-04-23 16:15:44 -0700

    Linked data screens and configure device screens. Note: configure device screens are all the same.
    Changes to be committed:
    	modified:   intPrototypePhone/src/FitbitButton.js
    	modified:   intPrototypePhone/src/screens.js

--- intPrototypePhone/src/FitbitButton.js
+++ intPrototypePhone/src/FitbitButton.js
@@ -6,29 +6,31 @@ exports.FitbitButton = BUTTONS.Button.template(function($){ return{
 								//new Label({top: 10, bottom: 10, left: 10, right: 10, string:"A&D Blood", style: headerStyle}),
 								//new Label({top: 10, bottom: 10, left: 10, right: 10, string:"Pressure Monitor", style: headerStyle}),
 								//new Label({top: 10, bottom: 10, left: 10, right: 10, string:"Bluetooth Enabled BP monitor", style: textStyle}),
-								new Label({top: 10, bottom: 10, left: 10, right: 10, string:"Fitbit Button", style: textStyle}),
+								new Label({top: 10, bottom: 10, left: 10, right: 10, string:"", style: textStyle}),
 						],
 				behavior: 
 				
 				//Object.create((MainScreen.behaviors[3]).prototype),
 
 				Object.create(BUTTONS.ButtonBehavior.prototype, {
-					onCreate : { value: function(content){
-						this.downSkin = greenPressSkin;
-						this.upSkin = greenSkin;
-					}},				
+					onCreate: { value: function(content, name){
+						content.first.string = name;
+					}},
 					onTouchBegan: { value: function(content){
-							content.skin = this.downSkin;
-							trace("Fitbit was tapped.\n");
-							}},
+						content.skin = this.downSkin;
+						trace("backButton was tapped.\n");
+					}},
 					onTouchEnded: { value: function(content){
 						content.skin = this.upSkin;
+						var oldScreen = currentScreen; //store the old screen so we can use it in the transition
+						currentScreen = new SCREENS.screen15; //make the new screen we want to transition to
 						
-						}},
+						//fire the transition between the oldScreen and the newScreen
+						application.run(new TRANSITIONS.CrossFade(), oldScreen, currentScreen, {duration: 100});
+					}},
 					onComplete: { value: function(content, message, json){
-							}},
-					})
-					}
-				});
+					}}
+				})
+}});
 
 	
