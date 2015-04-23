298cc7cbb4503d1cb7fabc9943eba90f6ac8e25f
Jesmin Ngo <xjesminngo@berkeley.edu>
2015-04-22 17:47:27 -0700

    merged all variables

--- intPrototypePhone/src/buttonLogin.js
+++ intPrototypePhone/src/buttonLogin.js
@@ -17,7 +17,7 @@ exports.LoginScreen = BUTTONS.Button.template(function($){
 							   [
 									Label($, 
 									{ 
-										left: 0, right: 0, top: 0, bottom: 0, style: buttonStyle, skin:greenSkin, 
+										left: 0, right: 0, top: 0, bottom: 0, style: textStyle, skin:greenSkin, 
 									    string: 'Returning User? Login', id : 1, 
 									}),									
 								], 
