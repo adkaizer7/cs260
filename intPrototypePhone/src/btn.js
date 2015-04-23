4e14fdd9988fb7eada6067db1d421c979045881c
Adarsh Mani <adarsh.mani@berkeley.edu>
2015-04-23 02:32:45 -0700

    Adding missing files
     Changes to be committed:
    	new file:   intPrototypePhone/src/btn.js
    	new file:   intPrototypePhone/src/btnPic.js
    	new file:   intPrototypePhone/src/screenBackButton.js

---
+++ intPrototypePhone/src/btn.js
@@ -0,0 +1,36 @@
+// KPR Script file
+var TRANSITIONS = require('transitions');
+
+exports.btn = BUTTONS.Button.template(function($){ return{
+	top: 10, bottom: 10, left: 10, right: 10, skin: $.skin, height : 60,
+	contents: [
+		new Line({top: 0, left: 0, right: 0, bottom: 0, contents: [
+			new Label({top: 0, bottom: 0, left: 5, right: 0, string: $.textForLabel, style: titleStyle}),
+		],
+		})
+	],	
+	behavior: Object.create(BUTTONS.ButtonBehavior.prototype, {
+		onCreate: { value: function(content){
+			this.upSkin = $.skin;
+			this.downSkin = $.darkSkin;
+			this.nextScreen = $.nextScreen;
+		}},
+		onTouchBegan: { value: function(content){
+			content.skin = this.downSkin;
+			trace("backButton was tapped.\n");
+		}},
+		onTouchEnded: { value: function(content){
+			content.skin = this.upSkin;
+			var oldScreen = currentScreen; //store the old screen so we can use it in the transition
+			currentScreen = new this.nextScreen; //make the new screen we want to transition to
+			
+			//fire the transition between the oldScreen and the newScreen
+			application.run(new TRANSITIONS.CrossFade(), oldScreen, currentScreen, {duration: 100});
+		}},
+		onComplete: { value: function(content, message, json){
+		}},
+	})
+}});
+
+
+// KPR Script file
