e410dbdcc9236180a0cbda022988a68497b78916
Jesmin Ngo <xjesminngo@berkeley.edu>
2015-04-22 23:05:40 -0700

    redesign view data

--- intPrototypePhone/src/BloodPressureScreenButton.js
+++ intPrototypePhone/src/BloodPressureScreenButton.js
@@ -3,7 +3,7 @@ var bp = 80;
 exports.BloodPressureScreenButton = BUTTONS.Button.template(function($){ return{
 	top:10, right: 10, left: 10, bottom: 10,  skin: greenSkin,
 	contents:[
-		new Label({left: 0, right: 0, top: 0, bottom: 0, string: bp + 'Hg mm', }),
+		new Label({left: 0, right: 0, top: 0, bottom: 0, string: bp + ' Hg mm', }),
 	],
 	behavior: Object.create((MainScreen.behaviors[10]).prototype),
 }});
